61cbf33cc64480f39f78ea5c598878c536109988
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2020-06-23 00:10:52 +0100

    small changes

--- dinos/src/app/app.module.ts
+++ dinos/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -15,4 +14,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
