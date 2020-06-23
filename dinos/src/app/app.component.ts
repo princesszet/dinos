7e2a1ade02ab509a7133d9fa0dd7ec1512cc2b38
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2020-06-24 00:08:05 +0100

    the table now displays dinosaurs yaaaay!

--- dinos/src/app/app.component.ts
+++ dinos/src/app/app.component.ts
@@ -13,5 +13,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Dinos';
+  pageTitle: string = 'Dinos';
 }
--- dinos/src/app/dinosaurs/dinosaur-list.component.ts
+++ dinos/src/app/dinosaurs/dinosaur-list.component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
 })
 
 export class DinosaurListComponent {
+    dinoTitle: string = "Dinosaur List";
+    dinosaurs: any[] = [
+        {
+            "id": 1,
+            "species": "Tyrannosaurus rex",
+            "height": "3.7 - 6.1 m",
+            "weight": "4,500 - 14,000 kg",
+            "foodSource": "carnivorous",
+            "starRating": 4.5,
+            "imageUrl": "assets/images/trex.jpeg"
+        },
+        {
+            "id": 2,
+            "species": "Brachiosaurus",
+            "height": "26 - 30 m",
+            "weight": "28.3 - 58 tons",
+            "foodSource": "herbivorous",
+            "starRating": 4.3,
+            "imageUrl": "assets/images/brachiosaurus.jpg"
+        },
+
+
+    ];
 
 }
