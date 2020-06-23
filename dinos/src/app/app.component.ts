362805f2b78f0c7a8f22a764e33064b65caffa9b
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2020-06-23 19:20:18 +0100

    created the dinosaur component

--- dinos/src/app/app.component.ts
+++ dinos/src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: `
+    <div><h1>{{ pageTitle }}</h1>
+      <app-dinos></app-dinos>
+    </div>
+  `
+  
 })
 
 export class AppComponent {
---
+++ dinos/src/app/dinosaurs/dinosaur-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-dinos',
+    templateUrl: './dinosaur-list.component.html'
+
+})
+
+export class DinosaurListComponent {
+
+}
