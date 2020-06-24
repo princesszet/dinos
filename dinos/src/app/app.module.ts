8f732bb808390c490b66527c48bef4dc3aefda48
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2020-06-24 23:42:39 +0100

    filtered by works!

--- dinos/src/app/app.module.ts
+++ dinos/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DinosaurListComponent } from './dinosaurs/dinosaur-list.component';
 
@@ -11,7 +12,8 @@ import { DinosaurListComponent } from './dinosaurs/dinosaur-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
