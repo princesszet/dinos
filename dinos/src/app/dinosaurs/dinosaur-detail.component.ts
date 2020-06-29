2c869f8df08ebbb62b6852d4b578a881f86a2571
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2020-06-29 13:28:49 +0100

    created the welcome component

--- dinos/src/app/dinosaurs/dinosaur-detail.component.ts
+++ dinos/src/app/dinosaurs/dinosaur-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { IDinosaur } from './dinosaur';
 
 @Component({
-  selector: 'app-dinosaur-detail',
   templateUrl: './dinosaur-detail.component.html',
   styleUrls: ['./dinosaur-detail.component.css']
 })
 export class DinosaurDetailComponent implements OnInit {
+  pageTitle: string = "Dinosaur Detail";
+  dinosaur: IDinosaur;
 
   constructor() { }
 
