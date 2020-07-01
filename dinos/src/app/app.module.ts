62d913a64276d2d84110731d8e2176f3e8f81c03
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2020-07-01 19:28:02 +0100

    new dinosaur module is now completed

--- dinos/src/app/app.module.ts
+++ dinos/src/app/app.module.ts
@@ -2,36 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { DinosaurListComponent } from './dinosaurs/dinosaur-list.component';
-import { StarComponent } from './shared/star.component';
-import { DinosaurDetailComponent } from './dinosaurs/dinosaur-detail.component';
 import { WelcomeComponent } from './home/welcome.component'
-import { DinosaurDetailGuard } from './dinosaurs/dinosaur-detail.guard';
 import { DinosaurModule } from './dinosaurs/dinosaur.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DinosaurListComponent,
-    StarComponent,
-    DinosaurDetailComponent,
     WelcomeComponent
   ],
 
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: "dinosaurs", component: DinosaurListComponent},
-      { path: "dinosaurs/:id", 
-        canActivate: [DinosaurDetailGuard], 
-        component: DinosaurDetailComponent},
       { path: "welcome", component: WelcomeComponent },
       { path: "", redirectTo: "welcome", pathMatch: "full" },
       { path: "**", redirectTo: "welcome", pathMatch: "full" }
--- dinos/src/app/dinosaurs/dinosaur.module.ts
+++ dinos/src/app/dinosaurs/dinosaur.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DinosaurListComponent } from './dinosaur-list.component';
+import { DinosaurDetailComponent } from './dinosaur-detail.component';
+import { DinosaurDetailGuard } from './dinosaur-detail.guard';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DinosaurListComponent,
+    DinosaurDetailComponent,
+    StarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: "dinosaurs", component: DinosaurListComponent },
+      {
+        path: "dinosaurs/:id",
+        canActivate: [DinosaurDetailGuard],
+        component: DinosaurDetailComponent
+      },
+    ])
   ]
 })
 
